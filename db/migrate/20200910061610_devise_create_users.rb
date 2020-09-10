6ae05994342b14a19b10467a644d2e720661e119
futa1952 <futa1952@icloud.com>
2020-09-10 16:46:38 +0900

    [作成]新規登録、ログイン機能

--- db/migrate/20200910061610_devise_create_users.rb
+++ db/migrate/20200910061610_devise_create_users.rb
@@ -32,8 +32,8 @@ class DeviseCreateUsers < ActiveRecord::Migration[5.2]
       # t.string   :unlock_token # Only if unlock strategy is :email or :both
       # t.datetime :locked_at
 
-      t.string :name, null: false
-      t.string :profile_image_id, null: false
+      t.string :name
+      t.string :profile_image_id
 
 
       t.timestamps null: false
