09121414f655a9c0ecb0ec459b20051cd5013629
futa1952 <futa1952@icloud.com>
2020-09-10 17:53:53 +0900

    [作成]footer,header

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,8 @@ Rails.application.routes.draw do
   devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root 'post_images#index'
+  get 'homes/top', to: 'homes#top'
+  get 'homes/about', to: 'homes#about'
   resources :users, only: [:show, :edit, :update]
 
 end
